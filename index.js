4339329d924bd0672da788e2c95ac0680fbd2292
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-02-24 00:49:20 +0100

    Started node.js thing

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello, world!');
+});
+var port = 8080
+server.listen(port);
+console.log("Listening on http:localhost:" + port)

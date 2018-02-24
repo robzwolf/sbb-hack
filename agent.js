cef9c670d0e80f2071d5f52c2971fd8f4bf86583
Luca Venier <luca.venier@rwth-aachen.de>
2018-02-24 19:55:07 +0100

    Tour is loaded via fetch and not hardcoded to the frontend anymore.

--- agent.js
+++ agent.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const sample = require('./sample');
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
@@ -9,8 +10,9 @@ router.use(function timeLog(req, res, next) {
 // define the home page route
 
 // define the about route
-router.get('/about', function(req, res) {
-  res.send('About birds')
+router.get('/tour', function(req, res) {
+  console.log('incoming request');
+  res.send(JSON.stringify(sample))
 })
 
 module.exports = router
--- app/app.js
+++ app/app.js
@@ -1,6 +1,22 @@
 var app = new Vue({
   el: '#app',
+  methods: {
+    getTour: function() {
+      const _this = this;
+      fetch('/agent/tour').then((data) => {
+        return data.json();
+      }).then((data) => {
+        console.log(data);
+        _this.tour = data.trip;
+      })
+    }
+  },
   data: {
-    customer_name: 'Michael Jackson'
+    customer_name: 'Michael Jackson',
+    tour: {
+      steps: [{
+        columns: []
+      }]
+    }
   }
 })

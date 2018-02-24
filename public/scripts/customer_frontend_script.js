33c80126ce7c72526c5a5ee5c6afe4f30c6341cb
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-02-24 10:40:40 +0100

    Fix broken link; implement selection on customer frontend

--- public/scripts/customer_frontend_script.js
+++ public/scripts/customer_frontend_script.js
@@ -1,3 +1,10 @@
 // customer_frontend_script.js
 
 console.log("Loaded custom JS")
+
+$(document).ready(() => {
+    console.log("doc ready")
+    $(".tile-activity").click(function() {
+        $(this).toggleClass("selected");
+    })
+})

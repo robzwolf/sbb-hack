0549b831479ca0f66afc0f37a9c87642eb498058
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-02-24 16:11:00 +0100

    Using Flask

--- algorithm/web_server.py
+++ algorithm/web_server.py
@@ -1,12 +1,20 @@
 # web_server.py
 
-import http.server
-import socketserver
+# import http.server
+# import socketserver
+#
+# PORT = 9090
+#
+# Handler = http.server.SimpleHTTPRequestHandler
+#
+# with socketserver.TCPServer(("", PORT), Handler) as httpd:
+#     print("web_server.py listening on port", PORT)
+#     httpd.serve_forever()
 
-PORT = 9090
+from flask import Flask
+app = Flask(__name__)
 
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("web_server.py listening on port", PORT)
-    httpd.serve_forever()
+@app.route("/")
+def hello():
+    return "Hello World!"

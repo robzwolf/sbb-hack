03c58bd5898b248a85b7ac3eb8ad4d3a2958cb37
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-02-24 07:30:05 +0100

    ejs

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 var port = 8080
-app.listen(port, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log("Example app listening on port %s!", port))

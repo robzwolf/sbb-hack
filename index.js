94aa2f5e618ec4081008c2e476e8099cef946009
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-02-24 08:17:46 +0100

    Started working on a bootstrap test page at /bootstrap

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static("public"));
 
-var port = 8080
-app.listen(port, () => console.log("Example app listening on port %s!", port))
+app.get("/", (req, res) => {
+    ejs.renderFile("views/index.ejs", {"txt": "Greetings!", "query": req.query}, function(err, str){
+        res.send(str);
+    });
+});
+
+app.get("/bootstrap", (req, res) => {
+    ejs.renderFile("views/bootstrap.ejs", {"query": req.query}, function(err, str){
+        res.send(str);
+    })
+})
+
+var port = 8080;
+app.listen(port, () => console.log("Example app listening on port %s!", port));

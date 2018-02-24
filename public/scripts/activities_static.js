81074813aaee0804bc96ce55abb792b1bc0bb727
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-02-24 11:52:37 +0100

    Lots of work on customer frontend

---
+++ public/scripts/activities_static.js
@@ -0,0 +1,12 @@
+{
+    "activities": {
+        "mountain-biking": "Mountain Biking",
+        "hiking": "Hiking",
+        "skiing": "Skiing",
+        "swimming": "Swimming",
+        "concerts": "Concerts",
+        "theatre": "Theatre",
+        "museums": "Museums",
+        "hackathons": "Hackathons"
+    }
+}
--- public/scripts/customer_frontend_script.js
+++ public/scripts/customer_frontend_script.js
@@ -3,8 +3,26 @@
 console.log("Loaded custom JS")
 
 $(document).ready(() => {
+    
     console.log("doc ready")
-    $(".tile-activity").click(function() {
-        $(this).toggleClass("selected");
-    })
-})
+    
+    // Load tile activities
+    $.getJSON("/scripts/activities_static.js", data => {
+        
+        console.log("did json", data);
+        
+        // Iterate through activities and append them to #tiles
+        $.map(data.activities, (activity, i) => {
+            console.log("appending", i, activity)
+            $("#tiles-like").append('<a href="#" id="tile-' + i + '" class="tile-activity tile-activity-like">' + activity + '</a>');
+            $("#tiles-dislike").append('<a href="#" id="tile-' + i + '" class="tile-activity tile-activity-dislike">' + activity + '</a>');
+        });
+        
+        // Add click handlers to new elements
+        $(".tile-activity").click(function(e) {
+            $(this).toggleClass("selected");
+            e.preventDefault();
+        })
+    });
+
+});

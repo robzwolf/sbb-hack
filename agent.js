f927c3f3c9ce2ebac16e23d6f68849a88047637b
Luca Venier <luca.venier@rwth-aachen.de>
2018-02-24 10:41:31 +0100

    created agent api on the backend

---
+++ agent.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+// define the home page route
+
+// define the about route
+router.get('/about', function(req, res) {
+  res.send('About birds')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const agent = require('./agent');
 
 app.use(express.static("public"));
 
+app.use('/agent', agent)
+app.use('/agent', express.static('app'))
+
 app.get("/", (req, res) => {
-    ejs.renderFile("views/index.ejs", {"txt": "Greetings!", "query": req.query}, function(err, str){
-        res.send(str);
-    });
+  ejs.renderFile("views/index.ejs", {
+    "txt": "Greetings!",
+    "query": req.query
+  }, function(err, str) {
+    res.send(str);
+  });
 });
 
 app.get("/customer_frontend", (req, res) => {
-    ejs.renderFile("views/customer_frontend.ejs", {"query": req.query}, (err, str) => {
-        res.send(str);
-    })
+  ejs.renderFile("views/customer_frontend.ejs", {
+    "query": req.query
+  }, (err, str) => {
+    res.send(str);
+  })
 })
 
 var port = 8080;
